48c85177f60809b1c813f5ff3a23149d8d6d04c9
moosah01 <moosa.hashim@nixorcollege.edu.pk>
2025-07-05 18:27:19 +0500

    use Effect Dependency practiced

--- react-movie-browser-js-mastery/src/App.jsx
+++ react-movie-browser-js-mastery/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,16 +7,23 @@ const Card = ({ title }) => {
   // inside useState(this indicates default value of variable)
   // each card manages its own hasLiked state for itself
   const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]); // this is an effect dependency, that only fire useEffect when hasLiked variable has changed
   return (
     <>
-      <div className="card">
-        <h2>{title}</h2>
-        <button
-          onClick={() => {
-            // toggled like state
-            setHasLiked(!hasLiked);
-          }}
-        >
+      <div
+        className="card"
+        // you take prevState to update and not the variable directly b/c react state updates are async and batched
+        // if you called setCount(count+1) multiple times, it may re-use stale values
+        onClick={() => setCount((prevState) => prevState + 1)}
+      >
+        <h2>
+          {title} <br />
+          {count}
+        </h2>
+        <button onClick={() => setHasLiked((prevLikeState) => !prevLikeState)}>
           {hasLiked ? "❤️" : "🤍"}
         </button>
       </div>
